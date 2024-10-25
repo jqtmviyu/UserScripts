dbacae23e30a7d2ec621beb9dece2126d5f83a6c
jqtmviyu <jqtmviyu@gmail.com>
2024-10-25 19:46:38 +0800

    fix: :bug: 旧版网页的回收站和分享记录用不了, 忽略掉

--- 百度网盘/baidupanbackup.user.js
+++ 百度网盘/baidupanbackup.user.js
@@ -2,11 +2,17 @@
 // @name        百度网盘回到旧版
 // @namespace   Violentmonkey Scripts
 // @match       https://pan.baidu.com/disk/main*
-// @version     0.2
+// @version     0.3
 // @author      -
 // @description 检测到ai助手,自动跳转回旧版网页
+// @downloadURL https://update.greasyfork.org/scripts/504409/%E7%99%BE%E5%BA%A6%E7%BD%91%E7%9B%98%E5%9B%9E%E5%88%B0%E6%97%A7%E7%89%88.user.js
+// @updateURL https://update.greasyfork.org/scripts/504409/%E7%99%BE%E5%BA%A6%E7%BD%91%E7%9B%98%E5%9B%9E%E5%88%B0%E6%97%A7%E7%89%88.meta.js
 // ==/UserScript==
 function redirectToHomeWithPath() {
+  // 旧版网页的回收站/分享用不了,排除掉
+  if (/^#\/(recyclebin|share)/.test(window.location.hash)) {
+      return; // 如果匹配任一前缀，则退出脚本
+  }
   // 获取当前页面的 URL
   const currentUrl = window.location.href
   // 使用 URL 构造函数解析当前 URL
